88efd3b90652a95e7792b9a99bc40a6ae69e0382
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2021-03-12 16:01:33 +0300

    Toggle form inputs based on selected button

--- src/components/BmiCalculator.vue
+++ src/components/BmiCalculator.vue
@@ -2,19 +2,71 @@
 export default {
   name: 'BmiCalculator',
   
+  data() {
+    return {
+      mass: '',
+      height: '',
+      feet:'',
+      inc:'',
+
+      metric: {
+        mass: 'kg',
+        height: 'cm',
+        select: false
+      },
+
+      us: {
+        mass: 'pound',
+        height: 'feet',
+        select: false
+      },
+    }
+  },
+
+  methods: {
+    calculateBmi() {
+      console.log(this.mass, this.height)
+    },
+
+    selectMetric() {
+      this.metric.select = !this.metric.select
+      this.us.select = false
+    },
+
+    selectUs() {
+      this.us.select = !this.us.select
+      this.metric.select = false
+    },
+  }
 }
 </script>
 
 <template>
   <section>
-    <div class="container">
-      <h1>Merve</h1>
+    <div>
+      <button @click="selectMetric">Metric</button>
+      <button @click="selectUs">US/UK Units</button>
     </div>
+    
+
+    <form v-if="metric.select">
+      <input type="number" v-model="mass" :placeholder="metric.mass">
+      <input type="number" v-model="height" :placeholder="metric.height">
+      <button @click.prevent="calculateBmi">Calculate</button>
+    </form>
+
+    <form v-if="us.select">
+      <h1>merve</h1>
+      <input type="number" v-model="mass" :placeholder="us.mass">
+      <input type="number" v-model="feet" placeholder="feet">
+      <input type="number" v-model="inc" placeholder="inc">
+      <button @click.prevent="calculateBmi">Calculate</button>
+    </form>
+    
   </section>
 </template>
 
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 h3 {
   margin: 40px 0 0;
